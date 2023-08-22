b51e88f7ce492a427c57317b649dfa4e8d9aeedc
Amr Samy <36173696+Amr-Samy@users.noreply.github.com>
2023-08-22 22:33:27 +0200

    enhanced feedback form validation
    returning the selected stars count

--- feedback/js/script.js
+++ feedback/js/script.js
@@ -1,7 +1,6 @@
-//////////////////////////// ! Rating //////////////////////////////////////
+//////////////////////////// ? Rating //////////////////////////////////////
 // Select all elements with the "i" tag and store them in a NodeList called "stars"
  const stars = document.querySelectorAll(".stars i");
-
  // Loop through the "stars" NodeList
  stars.forEach((star, index1) => {
    // Add an event listener that runs a function when the "click" event is triggered
@@ -14,9 +13,17 @@
      });
    });
  });
-//////////////////////////// ! charater counter //////////////////////////////////////
-
+//////////////////////////// ? charater counter //////////////////////////////////////
 $('textarea').keyup(function () {
     var characterCount = $(this).val().length,current = $('#current');
     current.text(characterCount);
-  });
+});
+//////////////////////////////////? Summit //////////////////////////////////////////
+document.getElementById("feedback_form").addEventListener("submit", feedbackSubmit);
+var stars_count=0;
+function feedbackSubmit() {
+  for (var i = 0; i < 5; i++){
+    stars[i].classList.contains('active') ? stars_count++ : stars_count=stars_count;
+  }
+  alert(`Your Feedback have been submitted sucssessfully with ${stars_count} stars`);
+}
