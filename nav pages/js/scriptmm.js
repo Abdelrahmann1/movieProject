900bcca0e9a2e14ca57facda5f0a1f27da0521a3
Amr Samy <36173696+Amr-Samy@users.noreply.github.com>
2023-09-01 22:06:32 +0200

    get Tickets

--- nav pages/js/scriptmm.js
+++ nav pages/js/scriptmm.js
@@ -180,26 +180,23 @@ var x;
 
     /////////////////////////////////////////// 
 
-    var topRated;
+    var nowPlaying;
     (async function getTopRated() {
         var data = await fetch("https://api.themoviedb.org/3/movie/now_playing?api_key=24ce3ad943eaffe233b9fe1d4450ba6c");
-        topRated = await data.json();
-    
-    
-        const favoriteCards = [];
+        nowPlaying = await data.json();
     
         for (let i = 0; i <4; i++) {
             
         var card = `<div class="card" >
         <div class="poster">
-            <img src="https://image.tmdb.org/t/p/w500${topRated.results[i].poster_path}">
+            <img src="https://image.tmdb.org/t/p/w500${nowPlaying.results[i].poster_path}">
            
         </div>
         <div class="details">
-         <i class="fa-regular fa-heart" id="favorite" ></i> 
-            <h2> ${topRated.results[i].title} </h2>
-            <p>${topRated.results[i].release_date}  </p>
-            <p id="rating">${topRated.results[i].vote_average}</p>
+            <i class="fa-solid fa-ticket" onclick="getTicket(${nowPlaying.results[i].title})"></i>
+            <h2> ${nowPlaying.results[i].title} </h2>
+            <p>${nowPlaying.results[i].release_date}  </p>
+            <p id="rating">${nowPlaying.results[i].vote_average}</p>
             <div id="tags"> <span>Action</span> <span>Drama</span> </div>
         </div>
         
@@ -208,4 +205,12 @@ var x;
             
             
         }
-        })();
+
+
+
+})();
+        
+
+function getTicket(title) {
+    console.log(toString(title));
+}
