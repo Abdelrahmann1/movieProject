d710f9a3de28da9d18b4b25ef1c2d1b4e8a053ed
Bilal Muhammed <belal9971@yahoo.com>
2023-09-07 00:12:28 +0300

    update UI and Carousal and Search

--- booking/script.js
+++ booking/script.js
@@ -65,7 +65,7 @@ var currentMovie;
     var data = await fetch("https://api.themoviedb.org/3/movie/now_playing?api_key=24ce3ad943eaffe233b9fe1d4450ba6c");
     nowPlaying = await data.json();
 
-    for (let i = 0; i < 11; i++) {
+    for (let i = 0; i < 16; i++) {
         movieList.innerHTML+=
                 `<option value=${i}>
             ${nowPlaying.results[i].title}
--- nav pages/js/script.js
+++ nav pages/js/script.js
@@ -91,15 +91,42 @@ $("#cardContainer").append(card);
 
 })();
 
-
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+   
 
     var playingNow;
 (async function nowPlaying(){ 
    var Response =await fetch("https://api.themoviedb.org/3/movie/now_playing?api_key=24ce3ad943eaffe233b9fe1d4450ba6c")
    playingNow = await Response.json();
     
-    
-$("#first-carousal").append(`<img src="https://image.tmdb.org/t/p/w500${playingNow.results[0].poster_path}" class="d-block w-100" alt="...">`)   
+   
+   $("#second-carousal").append(`<img style="filter: blur(2px)" src="https://image.tmdb.org/t/p/w500${playingNow.results[5].poster_path}" class="d-block w-100" " alt="...">`)  
+   $("#second-carousal").append(`<div class="carousel-caption">
+   <p class="h1 text-light shadow shadow-lg " style="text-shadow: 4px 4px black;" > ${playingNow.results[5].title} </p>
+   <button type="button" class="btn btn-danger btn-lg btn-block  my-5">Take the Movie Now $200</button>
+   <!-- <button type="button" class="btn btn-danger my-5 btn-lg">Large button</button> -->
+   <p class="bg-black p-2">${playingNow.results[5].overview} </p>
+   </div>`)  
+
+   $("#third-carousal").append(`<img style="filter: blur(2px)" src="https://image.tmdb.org/t/p/w500${playingNow.results[7].poster_path}" class="d-block w-100"  alt="..."> `)  
+   $("#third-carousal").append(`<div class="carousel-caption">
+<p class="h1 text-light shadow shadow-lg " style="text-shadow: 4px 4px black;" > ${playingNow.results[7].title} </p>
+
+<button type="button" class="btn btn-lg  btn-block btn-danger my-5" >Take the Movie Now $200</button>
+
+<!-- <button type="button" class="btn btn-danger my-5 btn-lg" style = "color:red">Large button</button> -->
+<p class="bg-black p-2">${playingNow.results[7].overview} </p>
+</div>`)   
+
+$("#first-carousal").append(`<img style="filter: blur(2px)" src="https://image.tmdb.org/t/p/w500${playingNow.results[3].poster_path}" class="d-block w-100"  alt="...">`)   
+$("#first-carousal").append(`<div class="carousel-caption">
+<p class="h1 text-light shadow shadow-lg " style="text-shadow: 4px 4px black  ;" > ${playingNow.results[3].title} </p>
+<button type="button" class="btn btn-danger btn-lg btn-block  my-5">Take the Movie Now $200</button>
+<!-- <button type="button" class="btn btn-danger my-5 btn-lg">Large button</button> -->
+<p class="bg-black  p-2">${playingNow.results[3].overview} </p>
+</div>`)   
     for (let i = 0; i < 18; i++) {
 
         
@@ -124,10 +151,20 @@ $("#NowPlayingContainer").append(`
 </div>`);
     }
 
+    let ticketButton = document.getElementsByClassName("btn-danger");
 
+    for (let i = 0; i < ticketButton.length; i++) {
+    
+        ticketButton[i].addEventListener("click", goToTickets);
+    }
+   
  
 })()
  
+function goToTickets() {
+    window.location = "../../booking/booking.html";
+    
+}
 
 // function takeTheMovie(obj) {
 //     alert(obj)
@@ -417,3 +454,259 @@ links.forEach(function(link) {
     link.classList.add('active');
   });
 });
+
+
+
+
+
+var inputElement = $("#movie-name")[0]
+
+var movieWrapper = $("#movie-wrapper")
+
+var input =  $(".form-control")[0];
+var myinput = document.getElementById("search")[0];
+console.log(movieWrapper)
+
+
+
+function loadingData() {
+    // localStorage.setItem("movieSearch",input.value);
+        const urlParams = new URLSearchParams(window.location.search);
+  const greetingValue = urlParams.get('searchValue');
+  console.log(greetingValue);
+}
+
+
+
+async function getSearch() {
+
+    
+
+//      sessionStorage.setItem("searchString",input.value);
+//    var searchString =sessionStorage.getItem("searchString");
+
+    const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZWI2ZGZlOTI5ZmVjM2M0YWMzYmQ4MzI2YTAzMzQyZSIsInN1YiI6IjY0NzMzYzIzZGQ3MzFiMmQ3OWQxODM5MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.yMM2Sh6hL8bC2f8Kv1pc7gZZ4BoHF4q8fRDnM3lTFdE'
+        }
+      };
+    //   console.log(input.value);
+      var mydata ;
+    var data = await fetch(`https://api.themoviedb.org/3/search/movie?query=${input.value}&include_adult=false&language=en-US&page=1`, options);
+    
+        mydata = await data.json();
+        console.log(mydata);
+
+
+      var mySearch = document.getElementById("SearchContainer");
+      mySearch.innerHTML= " ";
+    //   $("#SearchContainer").append(" ");
+    
+    function load() {
+        
+        
+    }
+        for (let i = 0; i < mydata.results.length; i++) {
+            
+            $("#SearchContainer").append(
+                `
+                <div class="card" onclick = "getTheMovie(this)">
+      <div class="poster">
+      <img src="https://image.tmdb.org/t/p/w500${mydata.results[i].poster_path}">
+      
+      </div>
+      <div class="details">
+      <i class="fa-solid" :"fa-regular"} fa-heart favorite"
+      id="favorite" 
+      
+      onclick= "toggleFav(${mydata.results[i].id},this)" ></i> 
+      
+      
+      <h2> ${mydata.results[i].title}  </h2>
+      <p>${mydata.results[i].release_date}  </p>
+      <p id="rating">${Number( mydata.results[i].vote_average).toFixed(1)}</p>
+      
+      <div id="tags"> <span id="firstgener">20</span> 
+       </div>
+      
+      </div>
+      
+      </div>
+
+                `
+            )
+            }}
+
+
+
+            //search
+
+            
+
+var inputElement = $("#movie-name")[0]
+
+var movieWrapper = $("#movie-wrapper")
+
+var input =  $(".form-control")[0];
+var myinput = document.getElementById("search")[0];
+console.log(movieWrapper)
+
+
+
+// function loadingData() {
+//     // localStorage.setItem("movieSearch",input.value);
+//         const urlParams = new URLSearchParams(window.location.search);
+//   const greetingValue = urlParams.get('searchValue');
+//   console.log(greetingValue);
+// }
+
+
+
+async function getSearch() {
+
+    
+
+//      sessionStorage.setItem("searchString",input.value);
+//    var searchString =sessionStorage.getItem("searchString");
+
+    const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZWI2ZGZlOTI5ZmVjM2M0YWMzYmQ4MzI2YTAzMzQyZSIsInN1YiI6IjY0NzMzYzIzZGQ3MzFiMmQ3OWQxODM5MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.yMM2Sh6hL8bC2f8Kv1pc7gZZ4BoHF4q8fRDnM3lTFdE'
+        }
+      };
+    //   console.log(input.value);
+      var mydata ;
+    var data = await fetch(`https://api.themoviedb.org/3/search/movie?query=${input.value}&include_adult=false&language=en-US&page=1`, options);
+    
+        mydata = await data.json();
+        console.log(mydata);
+
+
+      var mySearch = document.getElementById("SearchContainer");
+      mySearch.innerHTML= " ";
+    //   $("#SearchContainer").append(" ");
+    
+    function load() {
+        
+        
+    }
+        for (let i = 0; i < mydata.results.length; i++) {
+            
+            $("#SearchContainer").append(
+                `
+                <div class="card" onclick = "getTheMovie(this)">
+      <div class="poster">
+      <img src="https://image.tmdb.org/t/p/w500${mydata.results[i].poster_path}" alt="https://image.tmdb.org/t/p/w500${mydata.results[i].backdrop_path}" >
+      
+      </div>
+      <div class="details">
+      <i class="fa-solid" :"fa-regular"} fa-heart favorite"
+      id="favorite" 
+      
+      onclick= "toggleFav(${mydata.results[i].id},this)" ></i> 
+      
+      
+      <h2> ${mydata.results[i].title}  </h2>
+      <p>${mydata.results[i].release_date}  </p>
+      <p id="rating">${Number( mydata.results[i].vote_average).toFixed(1)}</p>
+      
+      <div id="tags"> <span id="firstgener">${getFirstGener(i,0)}</span> 
+       </div>
+      
+      </div>
+      
+      </div>
+
+                `
+            )
+
+    //           mySearch.innerHTML = `<div class="card" onclick = "getTheMovie(this)">
+    //   <div class="poster">
+    //   <img src="https://image.tmdb.org/t/p/w500${mydata.results[i].poster_path}">
+      
+    //   </div>
+    //   <div class="details">
+    //   <i class="fa-solid" :"fa-regular"} fa-heart favorite"
+    //   id="favorite" 
+      
+    //   onclick= "toggleFav(${mydata.results[i].id},this)" ></i> 
+      
+      
+    //   <h2> ${mydata.results[i].title}  </h2>
+    //   <p>${mydata.results[i].release_date}  </p>
+    //   <p id="rating">${Number( mydata.results[2].vote_average).toFixed(1)}</p>
+      
+    //   <div id="tags"> <span id="firstgener">20</span> 
+    //    </div>
+      
+    //   </div>
+      
+    //   </div>`;
+
+        }
+    
+
+        // .then(response =>  response.json() )
+        // .then(response => console.log(response)).then(
+        //     response => {
+        //       console.log("Hi");
+        //     }
+        // )
+        // .catch(err => console.error(err));  
+        
+        // console.log( data.json())
+            // var x = await data.json();
+            // console.log(x);
+        // for (let i = 0; i < response.results.length; i++) {
+        //     const element = array[i];
+            
+        // }
+    
+}
+
+
+
+
+// $("#search-btn").click( function(){
+//     movieWrapper.html(" ")
+//     $("#error").text("")
+//     $("#loader").text("loading........")
+// $.get(`https://api.themoviedb.org/3/search/movie?query=${inputElement.value}&include_adult=false&language=en-US&page=1` , function(response){
+//     console.log(response);
+//     console.log(response.results[0].title);
+
+
+
+
+        
+//     $("#loader").text(" ")
+//     var movies = response.Search ;
+//     console.log(movies.results[1].title);
+//     for( var i = 0 ; i < 18 ; i++ ){
+//         console.log(response.results[i].title)
+//         var movieCard = $("<div>")
+//         movieCard.addClass("movie-card")
+//         var movieImage = $("<img>")
+//         movieImage.attr("src" ,  ` https://image.tmdb.org/t/p/w500${response.results[i].poster_path} `)
+//         movieImage.addClass("movie-image")
+//         var titleElement = $("<p>")
+//         titleElement.text(response.results[i].title)
+//         var type = $("<p>")
+//         type.text(movies[i].Type)
+//         var releaseYear = $("<p>")
+//         releaseYear.text(response.results[i].release_date)
+//         movieCard.append(movieImage,titleElement,type,releaseYear)
+//         movieWrapper.append(movieCard)
+
+//      }
+// }   )
+// } )
+function test() {
+   
+  var x = localStorage.getItem("movieSearch");
+    alert(x);
+}
