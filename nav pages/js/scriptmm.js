0e071337a28b90f0560e93a3092007e146b067b7
Amr Samy <36173696+Amr-Samy@users.noreply.github.com>
2023-09-09 15:03:45 +0200

    nav pages finished

--- nav pages/js/scriptmm.js
+++ nav pages/js/scriptmm.js
@@ -19,8 +19,8 @@ var genersList = {
      10752:"War",
      37:"Western",
 }
-function getFirstGener(i,x) {  
-    return genersList[topRated.results[i].genre_ids[x]];
+function getFirstGener(i,x,list) {  
+    return genersList[list.results[i].genre_ids[x]];
     }
 
 let btn2 = document.getElementById("btn2");
@@ -41,9 +41,6 @@ btn2.onclick=function(){
         behavior:"smooth"
     })
 };  
- ////////////////////////////////// 
-
-
 
 
 
@@ -64,7 +61,9 @@ var x;
         <h2> ${x.results[i].title}  </h2>
         <p>${x.results[i].release_date}  </p>
         <p id="rating">${x.results[i].vote_average}</p>
-        <div id="tags"> <span>${getFirstGener(i,0)}</span> <span>Drama</span> </div>
+        <div id="tags">
+        ${getFirstGener(i, 0, x) !== undefined ? ` <span>${getFirstGener(i, 0, x)}</span>` : ""} 
+        ${getFirstGener(i, 1, x) !== undefined ?` <span>${getFirstGener(i, 1, x)}</span>` : ""} 
     </div>
     
     </div>` ;
@@ -74,74 +73,6 @@ var x;
     })();
 
 
-   
-// $(document).ready(function(){
-//     var favoriteIcon = document.getElementById('favorite');
-//     favoriteIcon.addEventListener('click', addToFavorites);
-//     function  addToFavorites(event) {
-//        const card = event.target.parentNode.parentNode;
-//        favoriteCards.push(card);
-//        console.log(card);
-//        console.log(favoriteCards);
-//        // Add code to save the card as a favorite
-//      }
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-   
-    // var card = ` <div class="card">
-    //     <div class="poster">
-    //         <img src="https://image.tmdb.org/t/p/w500">
-           
-    //     </div>
-    //     <div class="details">
-    //      <i class="fa-regular fa-heart" id="favorite" ></i> 
-    //         <h2> ${finalResults.results[0].title}  </h2>
-    //         <p> Directed By quantin Tarantino </p>
-    //         <p id="rating">7.5</p>
-    //         <div id="tags"> <span>Action</span> <span>Drama</span> </div>
-    //     </div>
-        
-    //     </div>`;
-
-    
-    
-  
-
-
-
-
- 
-// var isSelected = false;
-    
-
-
-
-
-
-
-
-
-
- 
 (async function getTopRated() {
     var data = await fetch("http://api.themoviedb.org/3/movie/top_rated?api_key=24ce3ad943eaffe233b9fe1d4450ba6c");
     topRated = await data.json();
@@ -161,7 +92,9 @@ var x;
         <h2> ${topRated.results[i].title} </h2>
         <p>${topRated.results[i].release_date}  </p>
         <p id="rating">${Number(topRated.results[i].vote_average).toFixed(1)}</p>
-        <div id="tags"> <span>${getFirstGener(i,0)}</span> <span>${getFirstGener(i,1)}</span> </div>
+        <div id="tags">
+        ${getFirstGener(i, 0, topRated) !== undefined ?` <span>${getFirstGener(i, 0, topRated)}</span>` : ""} 
+        ${getFirstGener(i, 1, topRated) !== undefined ?` <span>${getFirstGener(i, 1, topRated)}</span>` : ""} 
     </div>
     
     </div>` ;
@@ -193,7 +126,9 @@ var x;
         <h2> ${topRated.results[i].title} </h2>
         <p>${topRated.results[i].release_date}  </p>
         <p id="rating">${Number(topRated.results[i].vote_average).toFixed(1)}</p>
-        <div id="tags"> <span>${getFirstGener(i,0)}</span></div>
+        <div id="tags">
+        ${getFirstGener(i, 0, topRated) !== undefined ? ` <span>${getFirstGener(i, 0, topRated)}</span>` : ""} 
+        ${getFirstGener(i, 1, topRated) !== undefined ?` <span>${getFirstGener(i, 1, topRated)}</span>` : ""} 
     </div>
     
     </div>` ;
@@ -225,22 +160,13 @@ var x;
             <h2> ${nowPlaying.results[i].title} </h2>
             <p>${nowPlaying.results[i].release_date}  </p>
             <p id="rating">${Number(topRated.results[i].vote_average).toFixed(1)}</p>
-            <div id="tags"> <span>${getFirstGener(i,0)}</span> <span>Drama</span> </div>
-        </div>
+            <div id="tags">
+        ${getFirstGener(i, 0, nowPlaying) !== undefined ? ` <span>${getFirstGener(i, 0, nowPlaying)}</span>` : ""} 
+        ${getFirstGener(i, 1, nowPlaying) !== undefined ?` <span>${getFirstGener(i, 1, nowPlaying)}</span>` : ""} 
+    </div>
         
         </div>` ;
             $(".cards4").append(card);
         }
-            
         }
-
-
-
 )();
-        
-
-function getTicket(title) {
-    console.log(toString(title));
-}
-
-
